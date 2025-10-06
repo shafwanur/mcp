d451fa9322401280bc580de5c13e3b178a731668
shaf <kazi.rahman@congatec.com>
2025-10-06 14:01:28 +0200

    neat trick to add all docker mcps from the catalog in one go

--- shaf/client.py
+++ shaf/client.py
@@ -1,5 +1,5 @@
 import os
-import dotenv 
+import dotenv
 import asyncio
 
 from dotenv import load_dotenv
@@ -10,44 +10,48 @@ load_dotenv()
 
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
 
+
 async def test():
     mcp_server_config = {
-    "mcpServers": {
-        "docs": { # this one runs locally. 
-        "command": "C:\\Users\\kazirahman\\.local\\bin\\uv",
-        "args": [
-            "--directory",
-            "C:\\Users\\kazirahman\\Documents\\mcp-research-noah\\shaf",
-            "run",
-            "docs.py"
-        ]
-        },
-        "calculator": {
-            "command": "docker",
-            "args": [
-                "run",
-                "--rm",
-                "-i",        
-                "calculator-mcp-server"
-            ]
+        "mcpServers": {
+            "docs": {  # this one runs locally.
+                "command": "C:\\Users\\kazirahman\\.local\\bin\\uv",
+                "args": [
+                    "--directory",
+                    "C:\\Users\\kazirahman\\Documents\\mcp-research-noah\\shaf",
+                    "run",
+                    "docs.py",
+                ],
+            },
+            "calculator": {
+                "command": "docker",
+                "args": ["run", "--rm", "-i", "calculator-mcp-server"],
+            },
+            "http": {"url": "http://localhost:8080/mcp"},
         }
-    }
     }
 
     client = MCPClient.from_dict(mcp_server_config)
 
     llm = ChatAnthropic(model="claude-sonnet-4-20250514")
 
-    # TODO: why is fetch_content disabled, what is it? 
-    agent = MCPAgent(llm=llm, client=client, disallowed_tools=["fetch_content"], use_server_manager=False, max_steps=30)
+    # TODO: why is fetch_content disabled, what is it?
+    agent = MCPAgent(
+        llm=llm,
+        client=client,
+        disallowed_tools=[],
+        use_server_manager=False,
+        max_steps=30,
+    )
 
     result = await agent.run(
-            """
+        """
     Führe bitte folgende Schritte aus:
     1. add two numbers, 33 and 22.
-
+    2. on duckduckgo, find what the capital of chile is, and how big it is. 
     """
     )
+
 
 if __name__ == "__main__":
     # Run the appropriate example
